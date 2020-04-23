7a2ce23c28170a0a29e20df12e8a49a07f835c14
fadhlanfm <fafadhlanriz@gmail.com>
2020-04-23 21:42:15 +0700

    add homepage

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router()
 const HomeController = require('../controllers/home')
 
-route.get('/', HomeController.getHome)
+router.get('/', HomeController.getHome)
 
-route.get('/*', HomeController.notFound)
+router.get('/*', HomeController.notFound)
 
 module.exports = router
