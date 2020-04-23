adf4752421298a8b09ed354110396b44fd3b93f2
fadhlanfm <fafadhlanriz@gmail.com>
2020-04-24 02:49:02 +0700

    edit penamaan dikit

--- routes/admin.js
+++ routes/admin.js
@@ -6,8 +6,8 @@ route.get('/', admin.showBooks)
 route.get('/add', admin.showAddBookForm)
 route.post('/add', admin.addBookProcess)
 
-route.get('/edit/:id', admin.showEditBookProcess)
-route.post('/edit/:id', admin.showEditBookProcess)
+route.get('/edit/:id', admin.showEditBookForm)
+route.post('/edit/:id', admin.editBookProcess)
 
 route.get('/delete/:id', admin.deleteBook)
 
